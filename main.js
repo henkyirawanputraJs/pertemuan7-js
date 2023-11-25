bbe95275d83fbe107b5c647b0f12c81e9fe24472
henkyirawanputraJs <henkyirawanputra6@gmail.com>
2023-11-24 20:57:35 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//for(let i = 0; i < 20; i++){
+ //   console.log("<p>Saya berada dikelas kafekoding hari ini")
+//}
+
+
+// sebuah kondisi, dengan batasnya 20 kali, dengan menampilkan "saya berada dikelas kafekoding hari ini"
+// buatkanlah sebuah kondisi mulai dari 0 dengan batasnya : 15, dengan menampilkan "saya belajar ke 1,2......15 kali"
+
+for(let k = 0; k < 15; k++){
+    console.log("<p>Saya belajar ke-"+ k +"</p>")
+}
